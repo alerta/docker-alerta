03494888275097566bf940a8e610d05789bc071a
Nick Satterly <nick.satterly@guardian.co.uk>
2014-08-08 01:49:06 +0100

    minor fixes

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 DOCKER_HOST="tcp://54.76.228.10:4242" 
 
 AUTH_REQUIRED=True
@@ -6,4 +8,4 @@ REDIRECT_URL=http://docker.alerta.io:49900/oauth2callback.html
 ALLOWED_EMAIL_DOMAIN=guardian.co.uk
 
 docker run --link alerta-db:mongo -e AUTH_REQUIRED=$AUTH_REQUIRED -e CLIENT_ID=$CLIENT_ID -e REDIRECT_URL=$REDIRECT_URL -t -i -p 49900:80 $*
-# docker run --link alerta-db:mongo -t -i -p 49900:80 $*
+
