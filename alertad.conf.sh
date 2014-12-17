1df06dfae4068737a02056c3dfb29e25c31f6bbf
Nick Satterly <nick.satterly@guardian.co.uk>
2014-12-17 23:18:04 +0000

    make config consistent

--- alertad.conf.sh
+++ alertad.conf.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 cat >/alertad.conf << EOF
-
 DEBUG = True
-USE_STDERR = True
-LOG_FILE = None
+
 SECRET_KEY = '$(< /dev/urandom tr -dc A-Za-z0-9_\!\@\#\$\%\^\&\*\(\)-+= | head -c 32)'
 AUTH_REQUIRED = $AUTH_REQUIRED
 OAUTH2_CLIENT_ID = '$CLIENT_ID'
 ALLOWED_EMAIL_DOMAINS = ['$ALLOWED_EMAIL_DOMAIN']
-PLUGINS = ['']
 
+PLUGINS = ['reject']
 EOF
