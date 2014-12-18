92354d96d67786157269b30c29f6c28678be0a48
Nick Satterly <nick.satterly@guardian.co.uk>
2014-12-18 23:48:29 +0000

    add /api to endpoint config

--- config.js.sh
+++ config.js.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 cat >/app/config.js << EOF
-
 'use strict';
-
 angular.module('config', [])
   .constant('config', {
-    'endpoint'    : "", // Use empty string when app served from same origin
+    'endpoint'    : "/api",
     'client_id'   : "$CLIENT_ID",
     'redirect_url': "$REDIRECT_URL"
   });
