b56c541e16b35ec0396066bd00339f48075f9b79
Nick Satterly <nick.satterly@guardian.co.uk>
2016-03-01 00:03:28 +1000

    Fix missing comma in config.js.sh and add example

--- config.js.sh
+++ config.js.sh
@@ -2,12 +2,13 @@
 
 cat >/app/config.js << EOF
 'use strict';
+
 angular.module('config', [])
   .constant('config', {
     'endpoint'    : "/api",
     'provider'    : "$PROVIDER",
     'client_id'   : "$CLIENT_ID",
-    'gitlab_url'  : "$GITLAB_URL"
+    'gitlab_url'  : "$GITLAB_URL",
 
     'colors'      : {}
   });
