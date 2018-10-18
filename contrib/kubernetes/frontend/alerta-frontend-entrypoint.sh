aa8576f57ad98380d1894956007c777b8dc23cb5
danudell-trustnetworks <dan.udell@trustnetworks.com>
2018-10-18 12:40:58 +0100

    Upgrade k8s contrib to v6 (#94)
    * Update Dockerfile
    
    * install pip packages with virtual env and permissions for the alerta user
    
    * Update to use new config file.json rather than .js
    
    * add port to uswigi

--- contrib/kubernetes/frontend/alerta-frontend-entrypoint.sh
+++ contrib/kubernetes/frontend/alerta-frontend-entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-if ! echo -n "${ALERTA_API_SERVER}" | grep -q '^".*"$'; then
-  ALERTA_API_SERVER="\"${ALERTA_API_SERVER}\""
+# Generate web console config, if not supplied
+if [ ! -f "${ALERTA_WEB_CONF_FILE}" ]; then
+  envsubst < /usr/share/nginx/html/config.json.template > /usr/share/nginx/html/config.json
 fi
-
-envsubst < /usr/share/nginx/html/config.js.template > /usr/share/nginx/html/config.js
 
 exec "$@"
