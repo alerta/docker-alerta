0769d5f65413d53a93fef01404d5f17c0a2502e3
Doodle <dan.udell@trustnetworks.com>
2018-10-18 09:57:29 +0000

    Update to use new config file.json rather than .js

--- contrib/kubernetes/backend/docker-entrypoint.sh
+++ contrib/kubernetes/backend/docker-entrypoint.sh
@@ -11,20 +11,16 @@ EOF
 fi
 
 if [ ! -f "${RUN_ONCE}" ]; then
-  # Init admin users and API Keys
+  
+  # Init admin users and API keys
   if [ -n "${ADMIN_USERS}" ]; then
     alertad user --password ${ADMIN_PASSWORD:-alerta} --all
     alertad key --all
-  fi
 
-  # Generate alerta CLI config
-  API_KEY=`alertad keys 2>/dev/null | head -1 | cut -d" " -f1`
-  if [ -n "${API_KEY}" ]; then
-    cat >${ALERTA_CONF_FILE} << EOF
-[DEFAULT]
-endpoint = http://localhost:8080
-key = ${API_KEY}
-EOF
+    # Create user-defined API key, if required
+    if [ -n "${ADMIN_KEY}" ]; then
+      alertad key --username $(echo ${ADMIN_USERS} | cut -d, -f1)  --key ${ADMIN_KEY}
+    fi
   fi
 
   # Install plugins
@@ -37,6 +33,21 @@ EOF
   touch ${RUN_ONCE}
 fi
 
-envsubst < /app/uwsgi.ini.template > /app/uwsgi.ini
+# Generate client config, if not supplied
+if [ ! -f "${ALERTA_CONF_FILE}" ]; then
+  API_KEY=${ADMIN_KEY:-$(alertad keys 2>/dev/null | head -1 | cut -d" " -f1)}
+  if [ -n "${API_KEY}" ]; then
+    cat >${ALERTA_CONF_FILE} << EOF
+[DEFAULT]
+endpoint = http://localhost:8080${BASE_URL}
+key = ${API_KEY}
+EOF
+  else
+    cat >${ALERTA_CONF_FILE} << EOF
+[DEFAULT]
+endpoint = http://localhost:8080${BASE_URL}
+EOF
+  fi
+fi
 
 exec "$@"
