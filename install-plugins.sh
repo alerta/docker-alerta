860e4a5acb30ad9bcf9fa7543865990673e492f3
michael-chambers <56092496+michael-chambers@users.noreply.github.com>
2022-11-16 23:04:54 -0800

    fix install-plugins.sh

--- install-plugins.sh
+++ install-plugins.sh
@@ -2,5 +2,5 @@
 
 while read plugin version; do
   echo "Installing '${plugin}' (${version})"
-  /venv/bin/pip install git+https://github.com/alerta/alerta-contrib.git@${version}#subdirectory=${plugin}
+  pip install git+https://github.com/alerta/alerta-contrib.git@${version}#subdirectory=${plugin}
 done </app/plugins.txt
